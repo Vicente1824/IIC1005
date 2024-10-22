b9c892265f93684a9f65b0f289bfeae3ab467c86
Vicente1824 <1824vicente@gmail.com>
2024-10-22 16:41:20 -0300

    MOB-1.

---
+++ MOB-1/components/AssetExample.js
@@ -0,0 +1,30 @@
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={require('../assets/capybara.jpeg')} />
+      <Text style={styles.paragraph}>
+        Soy un Capybara que vive en Argentina.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  logo: {
+    height: 128,
+    width: 128,
+  }
+});
